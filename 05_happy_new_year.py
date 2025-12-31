76ca91d5fb6a94f4b10ea46f7977bd1eb4a28331
Vedika-Sd <vedikasardeshmukh7@gmail.com>
2026-01-01 00:30:37 +0530

    Happy New Year 2026

---
+++ 05_happy_new_year.py
@@ -0,0 +1,32 @@
+heart = [
+    "  **   **  ",
+    " ****** ** ",
+    " ********* ",
+    "  *******  ",
+    "   *****   ",
+    "    ***    ",
+    "     *     "
+]
+
+print("Happy New Year Vedu! 💖\n")
+
+
+print("\n2026 = Dream, Grind, Victory, Unity, Energy 2026!")
+
+fireworks = [
+    "     *     ",
+    "    ***    ",
+    "  *******  ",
+    "    ***    ",
+    "     *     "
+]
+import time
+print("🎉 HAPPY NEW YEAR 2026 🎉\n")
+
+for _ in range(3):
+    for line in heart + fireworks:
+        print(line)
+        time.sleep(0.2)
+    print("\n")
+
+print("\n2026 will bring lot of love and Success!🎉")
